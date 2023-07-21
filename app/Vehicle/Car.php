d18f9d176ea6ba50842dce369c4050e609f990ef
Denis Zarubin <marketcreatekrs@gmail.com>
2023-07-21 18:44:32 +0700

    add transports

---
+++ app/Vehicle/Car.php
@@ -0,0 +1,19 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Vehicle;
+
+use App\Abstract\Vehicle;
+
+class Car extends Vehicle
+{
+    public function __construct(string $model, int $speed, float $price)
+    {
+        parent::__construct($model, $speed, $price);
+        $this->nameVehicle = static::class;
+    }
+    public function getInfo(): string
+    {
+        return "{$this->nameVehicle}: " . $this->getModel() . ', ' . $this->getSpeed() . ' km/h, $' . (int)$this->getPrice();
+    }
+}
